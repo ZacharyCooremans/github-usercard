250768f146c2bfdd33c218155527553e26f1b081
Zachary Cooremans <zcooremans@gmail.com>
2021-03-11 14:11:24 -0600

    Making a commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios
+.get("https://api.github.com/users/ZacharyCooremans")
+.then()
+.catch()
 
+const test = axios.get('https://api.github.com/users/ZacharyCooremans');
+console.log(test)
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +56,50 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker (obj) {
+
+  // instantiating the elements
+  const card = document.createElement('div');
+  const img = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const h3 = document.createElement('h3');
+  const pUser = document.createElement('p');
+  const pLocation = document.createElement('p');
+  const pProfile = document.createElement('p');
+  const a = document.createElement('a');
+  const pFollowers = document.createElement('p');
+  const pFollowing = document.createElement('p');
+  const pBio = document.createElement('p');
+
+  //setting class name, attributes
+  card.classList = 'card';
+  cardInfo.classList = 'card-info';
+  h3.classList = 'name';
+  pUser.classList = 'username';
+  //pProfile
+  img.src = obj['avatar_url'];
+  pUser = obj['login']
+  pLocation = obj['location']
+  a = obj['url']
+  pFollowers = obj['followers']
+  pFollowing = obj['following']
+  pBio = obj['bio']
+
+  // setting hierarchy
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(h3);
+  cardInfo.appendChild(pUser);
+  cardInfo.appendChild(pLocation);
+  cardInfo.appendChild(pProfile);
+  pProfile.appendChild(a);
+  cardInfo.appendChild(pFollowing);
+  cardInfo.appendChild(pFollowing);
+  cardInfo.appendChild(pBio);
+
+  return card
+}
+console.log(cardMaker)
 
 /*
   List of LS Instructors Github username's:
